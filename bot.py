2191782b191483145db9425d84f5f703055ec11c
JasonKChow <jason.k.chow@vanderbilt.edu>
2022-05-22 12:34:47 -0500

    black formatting

--- bot.py
+++ bot.py
@@ -5,6 +5,22 @@ import json
 from RaidedGW2Bot import bot as gw2Bot
 
 
+class RaidedBot(commands.Bot):
+    def __init__(
+        self,
+        *args,
+        intents: discord.Intents,
+        **kwargs,
+    ):
+        super().__init__(*args, intents=intents, **kwargs)
+
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        serverID = discord.Object(id=940689261567557634)
+        self.tree.copy_global_to(guild=serverID)
+        await self.tree.sync(guild=serverID)
+
+
 class General(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
@@ -34,7 +50,16 @@ if __name__ == "__main__":
     intents.members = True
     intents.message_content = True
 
-    bot = commands.Bot(command_prefix="$", intents=intents)
-    bot.add_cog(General(bot))
-    bot.add_cog(gw2Bot.LogUploader(bot, gw2Bot.gw2.app.teamIDs))
-    bot.run(config["token"])
+    bot = RaidedBot(
+        command_prefix="$",
+        intents=intents,
+        application_id=config["application_id"],
+    )
+
+    async def setup(bot):
+        await bot.add_cog(General(bot))
+        await bot.add_cog(gw2Bot.LogUploader(bot, gw2Bot.gw2.app.teamIDs))
+        await bot.add_cog(eventMan.EventManager(bot))
+        await bot.start(config["token"])
+
+    asyncio.run(setup(bot))
